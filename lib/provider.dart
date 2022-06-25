55e8dd21dba00ca1d5bd5bb83bbd4d80803d99fe
Yoh <yokeus2000@gmail.com>
2022-06-25 12:42:36 +0900

    add: Providerファイルを作成

---
+++ lib/provider.dart
@@ -0,0 +1,7 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final infoTextStateProvider = StateProvider.autoDispose((ref) => '');
+
+final emailStateProvider = StateProvider.autoDispose((ref) => '');
+
+final passwordStateProvider = StateProvider.autoDispose((ref) => '');
