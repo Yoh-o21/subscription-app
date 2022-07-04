9a5539136c91f61145bf5fa9242472d81456ab21
Yoh <yokeus2000@gmail.com>
2022-07-04 19:01:22 +0900

    update: notifierへ変更

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:subscription_app/pages/sign_page.dart';
 import 'package:subscription_app/theme_controller.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await initializeDateFormatting("ja_JP", null);
 
   /// runApp Riverpod
   runApp(const ProviderScope(child: MyApp()));
@@ -17,14 +19,14 @@ class MyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final themeMode = ref.watch(themeModeProvider.state).state;
+    final themeMode = ref.watch(themeModeProvider.notifier).state;
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       themeMode: themeMode,
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
-      home: SignPage(),
+      home: const SignPage(),
     );
   }
 }
--- lib/theme_controller.dart
+++ lib/theme_controller.dart
@@ -4,6 +4,6 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 final themeModeProvider = StateProvider<ThemeMode>((ref) => ThemeMode.light);
 
 final isDarkModeProvider = Provider<bool>((ref) {
-  final themeMode = ref.watch(themeModeProvider.state).state;
+  final themeMode = ref.watch(themeModeProvider.notifier).state;
   return themeMode == ThemeMode.dark;
 });
