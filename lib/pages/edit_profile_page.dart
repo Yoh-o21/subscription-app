a3fed5d9fe90b740a4da2ac8a2d8de1954d91e73
Yoh <yokeus2000@gmail.com>
2022-07-05 01:30:17 +0900

    add: userName編集を追加

--- lib/pages/edit_profile_page.dart
+++ lib/pages/edit_profile_page.dart
@@ -1,18 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:subscription_app/user_provider.dart';
 
 class EditProfilePage extends ConsumerWidget {
   const EditProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(userStateNotifierProvider)!;
+    final userNameController = TextEditingController(text: user.userName);
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          icon: Icon(Icons.arrow_back),
+          icon: const Icon(Icons.arrow_back),
           onPressed: () {
             Navigator.pop(context);
           },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            //入力フォーム
+            TextField(
+              controller: userNameController,
+              decoration: const InputDecoration(labelText: "User Name"),
+            ),
+            const SizedBox(height: 8),
+            //Saveボタン
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  ref
+                      .read(userStateNotifierProvider.notifier)
+                      .update(userNameController.text);
+                  Navigator.pop(context);
+                },
+                child: const Text("Save"),
+              ),
+            ),
+          ],
         ),
       ),
     );
